ad9db0057fe7133b61046082d361fb5bf43afa4a
hijuliancode <hijuliansosa@gmail.com>
2021-06-14 15:57:28 -0500

    rxjs: Nuestro primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { Observable } from "rxjs";
+
+const obs$ = new Observable<string>(subs => {
+  subs.next('Hola')
+  subs.next('Mundo')
+
+  subs.next('Hola')
+  subs.next('Mundo')
+
+  subs.complete() // no emite nada a los subscribers despues de el complete
 
 
-console.log('Hola Mundo!');
+  subs.next('Hola')
+  subs.next('Mundo')
+})
 
-
-
-
-
-
-
+// obs$.subscribe(res => console.log(res))
+obs$.subscribe(console.log)
