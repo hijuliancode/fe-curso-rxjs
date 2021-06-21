13edf611405c81fdd6cd98343ce1a1a149ac76f9
hijuliancode <hijuliansosa@gmail.com>
2021-06-21 14:21:25 -0500

    rxjs: Operadores de tiempo: sampleTime

--- src/operadores-de-tiempo/03-sampleTime.ts
+++ src/operadores-de-tiempo/03-sampleTime.ts
@@ -1,1 +1,13 @@
-import { sampleTime } from 'rxjs/operators'
+import { fromEvent } from 'rxjs'
+import { map, sampleTime, tap } from 'rxjs/operators'
+
+// SAMPLETIME:
+// Emits the most recently emitted value from the source Observable within periodic time intervals.
+
+const click$ = fromEvent<MouseEvent>( document, 'click' )
+
+click$.pipe(
+  sampleTime(5000),
+  tap(x => console.log(x)),
+  map( ({x, y}) => ({x, y}) ),
+).subscribe( console.log )
