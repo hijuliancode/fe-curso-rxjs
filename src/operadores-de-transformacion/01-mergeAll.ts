7618d22b128aaa044687ccab008ba4ca64376e28
hijuliancode <hijuliansosa@gmail.com>
2021-06-26 21:03:15 -0500

    rxjs | mergeAll : tipando operadores

--- src/operadores-de-transformacion/01-mergeAll.ts
+++ src/operadores-de-transformacion/01-mergeAll.ts
@@ -1,6 +1,8 @@
-import { fromEvent } from 'rxjs'
+import { fromEvent, Observable } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
 import { debounceTime, map, mergeAll, pluck } from 'rxjs/operators'
+import { GithubUser } from '../interfaces/github-user.interface'
+import { GithubUsersResp } from '../interfaces/github-users.interface'
 
 const click$ = fromEvent( document, 'click').subscribe( console.log )
 
@@ -9,6 +11,30 @@ const textInput = document.createElement('input')
 const orderList = document.createElement('ol')
 
 body.append(textInput, orderList)
+
+// Helpers
+const mostrarUsuarios = ( usuarios: GithubUser[] ) => {
+  orderList.innerHTML = ''
+
+  console.log('usuarios', usuarios)
+
+  for(const usuario of usuarios) {
+    console.log('usuario', usuario)
+    const li  = document.createElement('li')
+    const img = document.createElement('img')
+    const anchor = document.createElement('a')
+
+    img.src       = usuario.avatar_url;
+    anchor.href   = usuario.html_url;
+    anchor.text   = 'Ver pagina';
+    anchor.target = '_blank';
+
+    li.append( img, (usuario.login + '' ), anchor)
+
+    orderList.append( li )
+  }
+}
+
 
 // Streams
 
@@ -31,13 +57,11 @@ const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup')
  */
 
  input$.pipe(
-  debounceTime(500),
-  pluck('target', 'value'),
-  map( text =>  ajax.getJSON(
+  debounceTime<KeyboardEvent>(500),
+  pluck<KeyboardEvent, string>('target', 'value'),
+  map<string, Observable<GithubUsersResp>>( text =>  ajax.getJSON(
     `https://api.github.com/search/users?q=${ text }`
   )),
-  mergeAll(), // Se suscribe y emite dichos valores, y cuando se completa, también se va a completar
-  pluck('items'),
-).subscribe( resp => {
-  console.log( resp )
-} )
+  mergeAll<GithubUsersResp>(), // Se suscribe y emite dichos valores, y cuando se completa, también se va a completar
+  pluck<GithubUsersResp, GithubUser[]>('items'),
+).subscribe( mostrarUsuarios )
